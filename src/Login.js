ae2c85a75c4974f8e41a53f77d2312cfe46baf1c
Wojciech Bandzerewicz <57405495+thewbuk@users.noreply.github.com>
2023-07-02 12:45:54 +0100

    add copy function to test values, react-toastify

--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { AiOutlineCopy, AiOutlineCheck } from 'react-icons/ai';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 class Login extends Component {
     state = {
         username: "",
         password: "",
-        error: ""
+        error: "",
+        testUsername: "test",
+        testPassword: "password123",
+        copiedUser: false,
+        copiedPass: false
     }
 
     handleInputChange = (event) => {
         this.setState({
             [event.target.name]: event.target.value
+        });
+    }
+
+    handleCopy = (type) => {
+        navigator.clipboard.writeText(this.state[type]);
+        this.setState({ [`copied${type.charAt(0).toUpperCase() + type.slice(1)}`]: true });
+        toast.success(`Copied ${type}!`, {
+            position: toast.POSITION.TOP_RIGHT
         });
     }
 
@@ -38,6 +53,7 @@ class Login extends Component {
     render() {
         return (
             <div className="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
+                <ToastContainer />
                 <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
                     <h1 className="font-bold text-center text-2xl mb-5">Login</h1> 
                     <div className="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
@@ -62,6 +78,20 @@ class Login extends Component {
                                 />
                                 <button type="submit" className="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-600 text-white w-full py-2.5 rounded-md text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block">Log in</button>
                             </form>
+                            <div className="py-5">
+                                <div className="gap-4 flex-col">
+                                    <div className='flex justify-between'>Username: {this.state.testUsername}
+                                        <button onClick={() => this.handleCopy('testUsername')} className="focus:outline-none">
+                                            {this.state.copiedUser ? <AiOutlineCheck className="text-green-500" /> : <AiOutlineCopy />}
+                                        </button>
+                                    </div>
+                                    <div className='flex justify-between'>Password: {this.state.testPassword}
+                                        <button onClick={() => this.handleCopy('testPassword')} className="focus:outline-none">
+                                            {this.state.copiedPass ? <AiOutlineCheck className="text-green-500" /> : <AiOutlineCopy />}
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
